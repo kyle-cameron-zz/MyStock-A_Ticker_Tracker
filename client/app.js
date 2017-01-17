da4b3ccb108fe8046d5efe2b0e46640d4002e087
Kyle Cameron <1kylecameron@gmail.com>
2017-01-17 15:30:06 -0500

    Auto-update.

--- client/app.js
+++ client/app.js
@@ -1,24 +1,48 @@
 angular.module('myApp', [])
 
-  .controller('TickerController', ['$scope', '$http', function($scope, $http){
+  .controller('TickerController', ['$scope', '$interval', '$http', function($scope, $interval, $http){
     $scope.newTicker = '';
     // $scope.getDatetime = new Date()
     $scope.tickers = [];
 
-    $scope.tickerSubmitter = function() {
+    $scope.tickerSubmitter = function() {/*$interval(function() {*/
+      // $interval(repeatFunction(), 2000);
+
       var newObj = {};
       newObj.short = $scope.newTicker.toUpperCase();
       var data = {newTicker: $scope.newTicker};
 
       $http.post('/api/tickerName', data)
-      .then(function(response){
+      .then((response) => {
         newObj.name = response.data;
         return $http.post('/api/tickerPrice', data);
       })
-      .then(function(response){
+      .then((response) => {
         newObj.price = response.data;
         newObj.time = new Date().toString('yyyy-MM-dd');
         $scope.tickers.push(newObj);
+        return $scope.tickers.forEach((e) => {
+          $interval( () => {
+            var data = {newTicker: e['short']};
+            return $http.post('/api/tickerName', data)
+            .then( response => {
+              e['name'] = response.data;
+              return $http.post('/api/tickerPrice', data);
+            })
+            .then( response => {
+              e['price'] = response.data;
+              e['time'] = new Date().toString('yyyy-MM-dd');
+            });
+          }, 2000)
+        })
       });
-    };
+
+
+    }
+
+
+
+
   }]);
+
+  // $interval(fn, delay, [count], [invokeApply], [Pass]);
